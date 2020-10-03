75042d82baa72c1362f24c4bf178e52a7e99022d
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-03 18:47:49 -0500

    called the button element in js and is correctly tracking clicks

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
-console.log('hello from js');
+console.log("hello from js");
 
 // event #2 when the document is ready
 $(document).ready(onReady);
 
-function()[
+function onReady(){
+    console.log("hello from jquery");
     // add evnt listeners aka click events
-
-
+    //get data to display
+    $('#Submit').on('click', buttonClicked);
+}
     
-] 
+function buttonClicked(){
+    console.log("I'm Clicked!");
     
-
+}
 
 
 // evntt #1
