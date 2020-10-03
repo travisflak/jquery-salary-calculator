e7f52cc7118371d4c5dcb0999dbdfcc7c7c086e3
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-03 18:30:26 -0500

    created a submit button in html

--- scripts.js
+++ scripts.js
@@ -3,12 +3,14 @@ console.log('hello from js');
 // event #2 when the document is ready
 $(document).ready(onReady);
 
-function onReady(number){
-    console.log('hello from jQuery', number);
+function()[
+    // add evnt listeners aka click events
+
+
     
-}
+] 
+    
 
 
 
 // evntt #1
-onReady(1);
