6a0d361f050346fa3c94f9208c5c041675989d62
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-03 16:07:30 -0500

    passed in a parameter to see if I can

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
 console.log('hello from js');
 
+// event #2 when the document is ready
 $(document).ready(onReady);
 
-function onReady(){
-    console.log('hello from jQuery');
+function onReady(number){
+    console.log('hello from jQuery', number);
     
 }
+
+
+
+// evntt #1
+onReady(1);
